3117d7f31be0a4bedf16c8956f7bd61784fb7f8c
MrBean1512 <davidmartin1512@gmail.com>
2020-11-28 18:59:20 -0800

    added some comments to the code

--- lib/task_list.dart
+++ lib/task_list.dart
@@ -2,26 +2,32 @@ import 'package:flutter/material.dart';
 
 import "file_handling.dart";
 
+//multiple pages implement ListViews so this is uesd for them all
+
+
+//builds a listView widget of tasks
 ListView buildTaskList() {
-  Map tasks = populateFromJson();
-  return (ListView.separated(
+  Map tasks = populateFromJson(); //Map is created from json file
+  return (ListView.separated( //begin construction of actual widget
     padding: const EdgeInsets.all(8),
-    itemCount: 4,
-    itemBuilder: (BuildContext context, int index) {
+    itemCount: 4, //number of widgets to create, used by index
+    itemBuilder: (BuildContext context, int index) {  //iterate through Map to make task objects
       return buildTaskBox(tasks['title'][index], tasks['description'][index],
           tasks['dateTime'][index]);
     },
-    separatorBuilder: (BuildContext context, int index) => const Divider(),
+    separatorBuilder: (BuildContext context, int index) => const Divider(), //just visually divides task boxes
   ));
 }
 
+//builds a taskBox container
+//intended to be used by buildTaskList()
 Container buildTaskBox(String title, String description, String dateTime) {
   Container taskBox = Container(
     padding: const EdgeInsets.all(32),
     color: Colors.green[50],
     child: Row(
       children: [
-        Icon(
+        Icon( //dart conveniently provides premade icons, this will be useful so 
           Icons.check_box,
           color: Colors.white,
         ),
@@ -32,14 +38,14 @@ Container buildTaskBox(String title, String description, String dateTime) {
               Container(
                 padding: const EdgeInsets.only(bottom: 8),
                 child: Text(
-                  title, //this is where the title variable is used
+                  title, //this is where the title string is used
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                   ),
                 ),
               ),
               Text(
-                description, //this is where the description variable is used
+                description, //this is where the description string is used
                 style: TextStyle(
                   color: Colors.grey[500],
                 ),
