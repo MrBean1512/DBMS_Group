bbec20f527815dff3e09b2610f3871c23ed31060
MrBean1512 <davidmartin1512@gmail.com>
2020-12-08 00:11:25 -0800

    calendar ui changes

--- lib/file_handling.dart
+++ lib/file_handling.dart
@@ -9,22 +9,43 @@ import 'dart:io';
 import 'dart:core';
 import 'dart:convert';
 
-//TODO
-//this will likely be a useful resource: https://dart.dev/tutorials/web/fetch-data
-//add functions to...
-//updata local data from remote database (alexis' stuff)
-//push new tasks to alexis' database
-//delete old tasks
-//due to some limitations of the dateTime variable in dart, editing an existing task will add a new task and delete the old one
-//handle shared tasks
-//leave currently shared tasks
-//add members to your current task
+/*
+TODO
+this will likely be a useful resource: https://dart.dev/tutorials/web/fetch-data
+sql
+  getQuery()
+    write to json
+  submitQuery()
+file handling
+  write query to a json file
+    maybe write the query to a map and then map to json
+  retrieve a map from the json file
+task managing
+  add a new task
+  edit an existing task
+    delete existing task and then add new one
+    complete a task, change the color, change description or title
+  delete a task
+finish ui
+  page today
+    add "today" header
+    only show today's tasks
+  page calendar
+    try to have it use the task Map
+    show task description and due time
+    optionally add editing features to it
+    optionally remove viewing options and insert calendar into list widget
+  page agenda
+    add '2 days' view button
+    add '1 week' view button
+    add 'category' view button
+      optionally use '2 week' view button if time doesn't permit this
+*/
 
 // Pre-fill the form with some default values. This is only temporary for the sake of code demonstration
 Map populateFromJson() {
   //final jsonDataAsString = new File('tasks.txt').readAsStringSync();
-  
-  
+
   final jsonDataAsString = '''{
       "id": [1, 2, 3, 4, 5],
       "title": ["these are dummy tasks", "do homework", "feed the dog", "call grandma", "start pork roast"],
@@ -32,7 +53,6 @@ Map populateFromJson() {
       "dateTime": ["2020-12-12 20:00:00.000", "2020-12-12 20:00:00.000", "2020-12-12 20:00:00.000", "2020-12-20 20:00:00.000", "2020-12-20 20:00:00.000"],
       "completed": [false, false, false, false, false]
   }''';
-  
 
   //sqlConnect();
 
@@ -75,14 +95,13 @@ Future sqlGetJsonTasks() async {
   });
   */
   var conn = await MySqlConnection.connect(ConnectionSettings(
-    host: 'localhost', 
-    port: 3306, 
-    user: 'admin', 
-    password: '#1Dmartin21', 
-    db: 'task_manager'));
+      host: 'localhost',
+      port: 3306,
+      user: 'admin',
+      password: '#1Dmartin21',
+      db: 'task_manager'));
 
-  var results = await conn.query(
-    'SELECT FROM task');
+  var results = await conn.query('SELECT FROM task');
   for (var row in results) {
     print('Name: ${row[0]}, email: ${row[1]}');
   }
@@ -90,25 +109,17 @@ Future sqlGetJsonTasks() async {
   conn.close(); // closing database connection here
 }
 
-Future sqlSubmit() async {
+Future sqlAddData(String query) async {
   //connection configuration to log into database
   MySqlConnection.connect(ConnectionSettings(
           host: 'localhost',
           port: 3306,
           user: 'admin',
           password: '#1Dmartin21',
-          db: 'Task'))
+          db: 'task_manager'))
       .then((conn) {
     //Run and execute a query
-    conn.query('SELECT Name FROM Task.tasks').then((results) {
-      List.from(results).forEach((row) {
-        // waits a certain amount of time before printing a result. Necessary to give the system
-        // enough time to do what it needs to before closing the program too early
-        Future.delayed(const Duration(milliseconds: 1000), () {
-          print('Name: ${row[0]}');
-        });
-      });
-
+    conn.query(query).then((results) {
       conn.close(); // closing database connection here
     });
   });
--- lib/page_calendar.dart
+++ lib/page_calendar.dart
@@ -41,72 +41,20 @@ class _PageCalendarState extends State<PageCalendar>
   void initState() {
     super.initState();
     final _selectedDay = DateTime.now();
-    DateTime initDay;
-    String initEvent;
-    
-    /*
-    _events = {
-      _selectedDay.subtract(Duration(days: 30)): [
-        'Event A0',
-        'Event B0',
-        'Event C0'
-      ],
-      _selectedDay.subtract(Duration(days: 27)): ['Event A1'],
-      _selectedDay.subtract(Duration(days: 20)): [
-        'Event A2',
-        'Event B2',
-        'Event C2',
-        'Event D2'
-      ],
-      _selectedDay.subtract(Duration(days: 16)): ['Event A3', 'Event B3'],
-      _selectedDay.subtract(Duration(days: 10)): [
-        'Event A4',
-        'Event B4',
-        'Event C4'
-      ],
-      _selectedDay.subtract(Duration(days: 4)): [
-        'Event A5',
-        'Event B5',
-        'Event C5'
-      ],
-      _selectedDay.subtract(Duration(days: 2)): ['Event A6', 'Event B6'],
-      _selectedDay: ['Event A7', 'Event B7', 'Event C7', 'Event D7'],
-      _selectedDay.add(Duration(days: 1)): [
-        'Event A8',
-        'Event B8',
-        'Event C8',
-        'Event D8'
-      ],
-      _selectedDay.add(Duration(days: 3)):
-          Set.from(['Event A9', 'Event A9', 'Event B9']).toList(),
-      _selectedDay.add(Duration(days: 7)): [
-        'Event A10',
-        'Event B10',
-        'Event C10'
-      ],
-      _selectedDay.add(Duration(days: 11)): ['Event A11', 'Event B11'],
-      _selectedDay.add(Duration(days: 17)): [
-        'Event A12',
-        'Event B12',
-        'Event C12',
-        'Event D12'
-      ],
-      _selectedDay.add(Duration(days: 22)): ['Event A13', 'Event B13'],
-      _selectedDay.add(Duration(days: 26)): [
-        'Event A14',
-        'Event B14',
-        'Event C14'
-      ],
-    };
-    */
+    DateTime date;
+    String event;
 
     for (var x = 0; x < tasks['dateTime'].length; x++) {
-      initDay = DateTime.parse(tasks['dateTime'][x]);
-      initEvent = tasks['dateTime'][x];
-      _events[initDay] = [initEvent];
+      date = DateTime.parse(tasks['dateTime'][x]);
+      event = tasks['title'][x];
+      if (_events[date] == null) {
+        _events[date] = [event];
+      } else {
+        _events[date].add(event);
+      }
       //_events[DateTime.parse(tasks['dateTime'][x])] = [tasks['dateTime'][x]];
+      //consider having the loop iterate through tasks instead
     }
-
 
     _selectedEvents = _events[_selectedDay] ?? [];
     _calendarController = CalendarController();
@@ -168,18 +116,21 @@ class _PageCalendarState extends State<PageCalendar>
       calendarController: _calendarController,
       events: _events,
       holidays: _holidays,
-      startingDayOfWeek: StartingDayOfWeek.monday,
+      startingDayOfWeek: StartingDayOfWeek.sunday,
+      daysOfWeekStyle: DaysOfWeekStyle(
+        weekendStyle: TextStyle(color: Colors.blue[400])),
       calendarStyle: CalendarStyle(
-        selectedColor: Colors.deepOrange[400],
-        todayColor: Colors.deepOrange[200],
-        markersColor: Colors.brown[700],
+        selectedColor: Colors.lightBlue[400],
+        todayColor: Colors.lightBlue[200],
+        markersColor: Colors.blue[800],
+        weekendStyle: TextStyle(color: Colors.blue[400]),  //0xFF2196F3
         outsideDaysVisible: false,
       ),
       headerStyle: HeaderStyle(
         formatButtonTextStyle:
             TextStyle().copyWith(color: Colors.white, fontSize: 15.0),
         formatButtonDecoration: BoxDecoration(
-          color: Colors.deepOrange[400],
+          color: Colors.lightBlue[400],
           borderRadius: BorderRadius.circular(16.0),
         ),
       ),
